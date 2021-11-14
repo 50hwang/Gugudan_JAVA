b8ba67ae5fcc91001f652aae8901516657b8b18d
Sooin Hwang <Sooin Hwang@DESKTOP-QQO099C>
2021-11-14 20:12:09 +0900

    Created Gugudan (2nd, 3rd)

---
+++ src/Gugudan.java
@@ -0,0 +1,26 @@
+// 출력 함수를 이용하여 구구단 2단과 3단 출력하기 
+public class Gugudan {
+	public static void main(String args[]) {
+		// 2단 - 타이핑 노가다
+		System.out.println(2 * 1);
+		System.out.println(2 * 2);
+		System.out.println(2 * 3);
+		System.out.println(2 * 4);
+		System.out.println(2 * 5);
+		System.out.println(2 * 6);
+		System.out.println(2 * 7);
+		System.out.println(2 * 8);
+		System.out.println(2 * 9);
+		System.out.println("");
+		// 3단 - Ctrl+Alt+아래화살표로 한 줄 duplicate
+		System.out.println(3 * 1);
+		System.out.println(3 * 2);
+		System.out.println(3 * 3);
+		System.out.println(3 * 4);
+		System.out.println(3 * 5);
+		System.out.println(3 * 6);
+		System.out.println(3 * 7);
+		System.out.println(3 * 8);
+		System.out.println(3 * 9);
+	}
+}
